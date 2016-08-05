250c219152a63e9246a07a9bba818420c68f5b82
ZachRiegel <zjriegel@gmail.com>
2016-08-04 23:36:50 -0400

    added haeder bar

--- templates.js
+++ templates.js
@@ -4,3 +4,17 @@ var searchCardTemplate = _.template(`
 		<div class="search-dummy">text</div>
 	</div>
 `);
+
+var headerTemplate = _.template(`
+	<div class="header-bar">
+		<div class="title">Fimbulwinter Feats:</div>
+		<div class="header-wrapper">
+			<a class="attribute-selector" href=#Fortitude>Fortitude</a>
+			<a class="attribute-selector" href=#Agility>Agility</a>
+			<a class="attribute-selector" href=#Combat>Combat</a>
+			<a class="attribute-selector" href=#Artistry>Artistry</a>
+			<a class="attribute-selector" href=#Cunning>Cunning</a>
+			<a class="attribute-selector" href=#Charisma>Charisma</a>
+		<div>
+	</div>
+`);
--- views.js
+++ views.js
@@ -4,8 +4,30 @@ var searchBarView = Backbone.View.extend({
 	},
 	render: function(){
 		this.$el.html(searchCardTemplate());
+		$(".search-bar").change( function(){
+			$(this).siblings(".search-dummy").html($(this).val())
+		})
+		$(".search-bar").keyup( function(){
+			$(this).change();
+		});
+	}
+});
+
+var headerView = Backbone.View.extend({
+	initialize: function(){
+		this.render();
+	},
+	render: function(){
+		this.$el.html(headerTemplate());
+		$(window).resize( function(){
+			//enforce header size bounds even if we resize
+			var width=Math.max(Math.min(1200,Math.floor((window.innerWidth*.9)/150)*150),200);
+			if (width > 900){
+				if (width < 1200){
+					width=900;
+				}
+			}
+			$(".header-bar").css("width", width);
+		});
 	}
 })
-
-
-
